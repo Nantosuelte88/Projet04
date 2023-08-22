073f0ea7b479456a494e028efeb62403d7f60284
Deborah Druine <deborahdruine@gmail.com>
2023-08-22 07:15:16 +0200

    Avant modification globale de la logique. Enlever ceertaines fonctions et les inclures dans la méthode Run

--- controllers/base.py
+++ controllers/base.py
@@ -5,7 +5,7 @@ from models.tournament import Tournament
 from models.match import Match
 from models.round import Round
 
-from data import PlAYERS
+from dataplayers import PlAYERS
 
 import random
 
@@ -19,7 +19,7 @@ class Controller:
         # mettre la vue içi
         self.players: List[Player] = []
         self.list_players = []
-        self.id_with_score = []
+        self.player_with_score = []
 
 
     def get_players(self):
@@ -28,7 +28,7 @@ class Controller:
             player = Player(i[0], i[1], i[2], i[3])
             self.players.append(player)
             self.list_players.append([player.name, player.first_name, player.date_birth, player.id_chess])
-        print("Print - get_player dans Controller - Len list_player :", len(self.list_players))
+#        print("Print - get_player dans Controller - Len list_player :", len(self.list_players))
 
 
     def __str__(self):
@@ -42,19 +42,18 @@ class Controller:
 #        print(info_tournament)
         score = 0
         for player in self.list_players:
-            id_player = player[3]
-            self.id_with_score.append([id_player, score])
-#        print(self.id_with_score)
+            self.player_with_score.append([player, score])
+ #       print(self.player_with_score)
         # verifier si il est mieux de ne prendre que l'id et le score ou garder les autres infos du joueur en stock içi ???
-        return info_tournament, self.id_with_score
+        return info_tournament, self.player_with_score
 
 
     def update_score(self):
         pass
 
     def sorted_players(self):
-        sorted_players_scores = sorted(self.id_with_score, key= lambda x: x[1], reverse=True)
- #       print("Select players for match : ", sorted_players_scores)
+        sorted_players_scores = sorted(self.player_with_score, key= lambda x: x[1], reverse=True)
+#        print("Select players for match : ", sorted_players_scores)
         return sorted_players_scores
 
 
@@ -68,12 +67,13 @@ class Controller:
             score1 = players[i][1]
             player2 = players[i + 1][0]
             score2 = players[i + 1][1]
-            print("player1 :", player1, "score1: ", score1, "player2", player2, "score2 :", score2)
+            print(init_round.name_round, "Print instanciation MATCH : (Dans boucle for)\n"
+                                         "player1 :", player1, "score1: ", score1, "player2", player2, "score2 :", score2)
             matchs = Match(player1, score1, player2, score2)
             #      print(matchs)
             init_round.list_matchs.append(matchs)
 
-        print(init_round)
+        print("Print après boucle for", init_round)
         result_match = []
         for p in init_round.list_matchs:
             random.shuffle(MATCH_SCORE)
@@ -81,11 +81,6 @@ class Controller:
             score2 = MATCH_SCORE[0][1]
             print("tututu", score1, score2)
         return init_round
-
-
-    def result_matchs(self, matchs):
-        for match in matchs:
-            print(match)
 
 
 
@@ -98,7 +93,7 @@ class Controller:
 
         self.new_round()
 
-        self.update_score()
+ #       self.update_score()
         self.sorted_players()
 
 
