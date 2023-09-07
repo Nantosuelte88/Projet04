bd36909b3753dfa718f59f02cbe9f43669a9eb72
Deborah Druine <deborahdruine@gmail.com>
2023-09-07 14:07:54 +0200

    si joueur en double, selection du bon joueur grace à son id -> ok. revoir logique ajout ou selection des joueurs quand on choisi un tournoi, logique de tournoi actif à revoir

--- views/tournamenentview.py
+++ views/tournamenentview.py
@@ -11,10 +11,14 @@ class View:
                            "Ajouter un joueur = 1\n"
                            "Ajouter un tournoi = 2\n"
                            "Choisir un tournoi = 3\n"
-                           "Quitter le menu = 4\n"                           
+                           "Quitter le menu = 4\n"
+                           "test = Capitalize\n"                           
                            "Votre choix : ")
             if choice.isnumeric() and int(choice) <=4:
                 break
+            elif choice.capitalize() == "Test":
+                print("good", choice)
+
             else:
                 print("Merci de rentrer une donnée valide")
         return choice
@@ -64,7 +68,7 @@ class View:
                             print("Veuillez entrer un format d'identifiant d'échec valide AB12345")
                     else:
                         print("Veuillez entrer un format d'identifiant d'échec valide AB12345")
-                add_players.append((name, last_name, date_birth, id_chess))
+                add_players.append((name.capitalize(), last_name.capitalize(), date_birth, id_chess.upper()))
 
             elif ask == "N":
                 break
@@ -75,39 +79,72 @@ class View:
 
     def choose_players(self, players):
         players_list = []
-        matching_list = []
-  #      print("Liste des joueurs : ")
-  #      for player in players:
-  #          print("Print choose player\n", player)
-  #          print("PRIIIIITNT du nom du joueur = ", player.name)
-
         while True:
             if players_list:
                 print("Joueur.s ajouté.s :", players_list)
+                for player in players_list:
+                    print(player.name, player.first_name, player.id_chess)
             add_player = input("Choisir un joueur ? O/N: ")
             if add_player == "O":
                 print("YEEES")
 
                 while True:  # 2eme while
 
-                    ask_name_player = input("Avec quels joueurs souhaitez-vous jouer ? ")
+                    ask_name_player = input("Entrer le nom du joueur ? ")
 
                     if all(char.isalpha() or char.isspace() for char in ask_name_player):
                         print("Verif joueur", ask_name_player)
 
+                        matching_list = []
+
+                        u = 0
                         for player in players:
-                            if ask_name_player == player.name:
-                                print("NOOOOM DU JOUEUUUUUR correspondant", ask_name_player, "=", player.name)
-                                print("Tous les joueurs correpondant ?", player)
+                            u += 1
+                            if ask_name_player.capitalize() == player.name:
+                                print(u, "NOOOOM DU JOUEUUUUUR correspondant", ask_name_player, "=", player.name)
+                                print(u, "Tous les joueurs correpondant ?", player)
                                 matching_list.append(player)
-                                if len(matching_list) >= 2:
-                                    print("plus de 2 correspondance dans la liste de joueurs. Ask_name =", ask_name_player, "player =", player, "matching_list = ", matching_list)
-                   #             players_list.append(ask_name_player)
 
                             else:
-                                print("Joueur inconnu", player.name)
+                                print(u, "Joueur inconnu", player.name)
+
+
+                        if len(matching_list) >= 2:
+                            print(u, "plus de 2 correspondance dans la liste de joueurs.", len(matching_list),
+                                  "Ask_name =", ask_name_player, "matching_list = ",
+                                  matching_list)
+                            print("Plusieurs joueurs portent ce nom de famille : ")
+                            o = 0
+
+                            for player in matching_list:
+                                o +=1
+                                print(o, player.name, player.first_name, player.id_chess)
+                            select_player = input("Merci d'indiquer l'ID du joueur que vous souhaitez ajouter: ")
+                            player_found = False
+                            f = 0
+
+                            for player in matching_list:
+                                f +=1
+                                if select_player.upper() == player.id_chess:
+                                    print(f, "verif id correct", player.name, player.first_name, player.id_chess)
+                                    players_list.append(player)
+                                    player_found = True
+                                    break
+
+                            if not player_found:
+                                print(f, "id incorrect")
+                            print(u, "fin du if")
+
+                        #             players_list.append(ask_name_player)
+                        elif len(matching_list) == 1:
+                            player = matching_list[0]
+                            print(u, "Correpondance normale", player.name, player.first_name)
+                            players_list.append(player)
+
                     else:
                         print("Merci d'entrer un nom de joueur valide")
+
+                    print("liste des joueurs : ", players_list)
 
                     another_player = input("Voulez ajouter un autre joueur? O/N? ")
                     if another_player == "N":
@@ -147,7 +184,7 @@ class View:
                     else:
                         print("Veuillez entrer un lieu valide sans chiffres ni caractères spéciaux.")
 
-                add_tournaments.append((name_tournament, locality))
+                add_tournaments.append((name_tournament.capitalize(), locality.capitalize()))
 
             elif ask == "N":
                 break
@@ -160,14 +197,15 @@ class View:
         print("Liste des tournois : ")
         for tournament in tournaments:
             print("Print select tournament\n", tournament)
-        ask_name_tournament = input("à quel tournoi souhaitez-vous jouer ?")
         while True:
+            ask_name_tournament = input("à quel tournoi souhaitez-vous jouer ?")
             if all(char.isalpha() or char.isspace() for char in ask_name_tournament):
                 print("Verif tournoi", ask_name_tournament)
                 break
             else:
                 print("Merci d'entrer un nom de tournoi valide")
-        return ask_name_tournament
+
+        return ask_name_tournament.capitalize()
 
 
 
